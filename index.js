8653789afe8a38e93c63a1609ebad28e1e15d6f7
james <ivanlavan773@gmail.com>
2025-06-11 08:30:51 +0300

    completed lab

--- index.js
+++ index.js
@@ -1,29 +1,61 @@
-const farmAnimals = 'cow horse sheep pig chicken';
+const animals = {
+  moo: "cow",
+  neigh: "horse",
+  baa: "sheep",
+  oink: "pig",
+  cluck: "chicken",
+};
 
-const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
+const { moo, neigh, baa, oink, cluck } = animals;
+
+const names = {
+  bessie: "cow",
+  dolly: "sheep",
+  babe: "pig",
+  little: "chicken",
+};
+
+const { bessie, dolly, babe, little } = names;
+
+const colors = {
+  blackAndWhite: "cow",
+  black: "sheep",
+  pink: "pig",
+};
+
+const { blackAndWhite, black, pink } = colors;
+const rainbowColors = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"];
+const [red, orange, yellow, green, blue, indigo, violet] = rainbowColors;
+
+const initials = ["red", "orange", "yellow", "green", "blue", "violet"];
+const [r, o, y, g, b, v] = initials;
+
+const singleColor = ["indigo"];
+const [indg] = singleColor;
 
 const muppet = {
-  muppetName: 'Miss Piggy',
-  color: 'pink',
-  song: 'Never Before, Never Again',
-  job: 'Cast member of The Muppet Show',
-  partner: 'Kermit'
+  muppetName: "Miss Piggy",
+  color: "pink",
+  song: "Never Before, Never Again",
+  job: "Cast member of The Muppet Show",
+  partner: "Kermit",
 };
 
-const nestedMuppet = {
-  nestedName: 'Kermit',
-  nestedColor: 'green',
-  album: {
-    theMuppetMovie: {
-      song1: 'Rainbow Connection',
-      song2: 'Moving Right Along',
-      song3: 'Never Before, Never Again',
-      song4: 'I Hope That Something Better Comes Along',
-    },
-  },
-  nestedJob: 'Host of The Muppet Show',
-  nestedPartner: 'Miss Piggy'
+const { muppetName, color, song, job, partner } = muppet;
+
+const muppetSongs = {
+  song2: "Moving Right Along",
+  song4: "I Hope That Something Better Comes Along",
 };
+
+const { song2, song4 } = muppetSongs;
+
+const muppetNested = {
+  nestedJob: "Host of The Muppet Show",
+  nestedPartner: "Miss Piggy",
+};
+
+const { nestedJob, nestedPartner } = muppetNested;
 
 // Strings
 
